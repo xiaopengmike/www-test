4a4964645b3e8ddc47369cca2bef7272ccd9bfc8
Mike <mike@Pro.local>
2019-06-14 12:28:21 +0800

    06

--- static/config.js
+++ static/config.js
@@ -11,7 +11,10 @@ const API = {
     recommendByQuery: janssenApi + '/v2/recommendByQuery',
 }
 let userData = {
+  // app_key: 'eeab85e7-c977-421d-a097-00d25b263bbd',
+  //正式服
   app_key: '19f63523-1ff7-45df-9830-2a62873c8af3',
+  //测试服
     user_id:1,
     wwid: ''
 }
